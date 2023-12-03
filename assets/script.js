68b9ce87d99b6044f71b347157710e317b55400e
Whitney Simpson <whitmoon4819@gmail.com>
2023-12-02 19:09:33 -0500

    second draft. weather api working, need to finish Acc. Crit.

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,43 @@
-var APIKey = "f5c7c99462c9401e774f3aa3a67ddc7f";
-var city = "Bellaire";
-// This section lists a number of parameters, but only the following two are required:
-// q: The query parameter, where we'll add the city variable.
-// appid: The application id or key, where we'll add the API key variable.
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
- city + "&appid=" + APIKey;
-//  http://api.openweathermap.org/data/2.5/weather is the base URL for calling the Current Weather Data API.
 
-//The question mark (?) marks the boundary between the base URL of the API call and the query terms of the API call.
+const apiKey = "6f20bf793d6b0fd27bc57a9f74fffa7e";
+function GetWeather(){
+    const newName= document.getElementById("cityInput");
+    const cityName= document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
 
-//As we mentioned earlier, q= is the query parameter, where we can add any user input to specify
-// the data that we want to request in the API call. The value assigned to this parameter is called the query string.
+fetch("https://api.openweathermap.org/data/2.5/forecast?q="+newName.value+"&units=imperial&appid=6f20bf793d6b0fd27bc57a9f74fffa7e")
+.then(response => response.json())
+.then(data => {
+    for(i=0;i<5;i++) {
+        document.getElementById("day" +(i+1)+ "Min").innerHTML ="Min:" +Number(data.list[i].main.temp_min)
+    }
+    for(i=0;i<5;i++) {
+    document.getElementById("day" +(i+1)+ "Max").innerHTML ="Max:" +Number(data.list[i].main.temp_max)
+    }
+    for(i=0;i<5;i++) {
+        document.getElementById("img" +(i+1)).src="https://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
+    }
+})
 
-//Following the query parameter, we concatenate the user input, which is stored in the variable city. 
-
-//This is the query string assigned to the query parameter.
-
-// The ampersand character (&) indicates that we're adding another parameter after the query parameter.
-
-// Next, we concatenate the other required parameter, appid=, where we'll add the API key specific to the application.
-
-// Finally, we concatenate the APIKey variable that contains the key we obtained at the beginning of this guide.
-
-//Now that you have created your query URL, you only need to call the Fetch API to pass the query URL in as a parameter, as shown in the following example:
-fetch(queryURL)
-//Remember that the query URL won't work automatically as it's written. You'll need to adjust your application to accept user input, to store in the city variable that you've created.
-async function getCurrentWeather() {
-    const response = await fetch(queryURL);
-    const currentWeather = await response.json();
-    console.log(currentWeather);
+.catch(err => alert("Something Went Wrong :/"))
 }
-getCurrentWeather();
 
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue ="Bellaire";
+    GetWeather();
+}
+
+const d = new Date();
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function checkDay(day){
+    if(day +d.getDay() >6) {
+        return day +d.getDay()-7;
+    } else {
+        return day +d.getDay();
+    }
+}
+
+for(i=0;i<5;i++) {
+    document.getElementById("day" +(i+1)).innerHTML = weekday[checkDay(i)];
+}
